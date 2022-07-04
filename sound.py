cc9d7f620e8a1b20221c634e1096e64c01f6b9f9
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-07-04 09:52:20 +0200

    Update sound.py

--- sound.py
+++ sound.py
@@ -1,22 +1,22 @@
-sampf = int(input('Enter sampling frequency: ')) # var
-sampress = int(input('Enter sampling resolution: ')) # var
-bitrate = sampf * sampress # calulate 
+sampf = int(input("Enter sampling frequency: "))  # var
+sampress = int(input("Enter sampling resolution: "))  # var
+bitrate = sampf * sampress  # calulate
 
-print('Bit rate: ',bitrate) # bit rate
+print("Bit rate: ", bitrate)  # bit rate
 
-duration =  int(input('Enter audio duration: ')) # var
-bits = duration * bitrate # calulate #
-totalbytes = bits // 8 # calulate
+duration = int(input("Enter audio duration: "))  # var
+bits = duration * bitrate  # calulate #
+totalbytes = bits // 8  # calulate
 
-print('Bits: ',bits, 'Bytes: ',totalbytes, 'Bytes') # bits and bytes
+print("Bits: ", bits, "Bytes: ", totalbytes, "Bytes")  # bits and bytes
 
-channels = int(input('Enter channels: ')) # var
-totalbytes = channels * totalbytes # calulate
+channels = int(input("Enter channels: "))  # var
+totalbytes = channels * totalbytes  # calulate
 
-if totalbytes >= 1000: # check
-  totalbytes = totalbytes // 10000 # calulate
-  if totalbytes < 1000: # check
-    print('File size: ',totalbytes, 'kb') # total bytes
-  else: 
-    totalbytes = totalbytes / 1000 # calulate
-    print('File size: ',totalbytes, 'kb') # total bytes
+if totalbytes >= 1000:  # check
+    totalbytes = totalbytes // 10000  # calulate
+    if totalbytes < 1000:  # check
+        print("File size: ", totalbytes, "kb")  # total bytes
+    else:
+        totalbytes = totalbytes / 1000  # calulate
+        print("File size: ", totalbytes, "kb")  # total bytes
